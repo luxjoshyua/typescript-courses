95929dedd8395a1d692b7f04efab9a0eba34401b
luxjoshyua <josh.e.fielding@gmail.com>
2024-12-31 14:14:20 +1100

    more ts

--- packages/chat/src/utils/error.ts
+++ packages/chat/src/utils/error.ts
@@ -5,7 +5,7 @@
  */
 function stringifyErrorValue(err: Error): string {
   return `${err.name.toUpperCase()}: ${err.message}
-  ${err.stack || '(no stack trace information)'}`
+  ${err.stack ?? '(no stack trace information)'}`
 }
 
 /**
@@ -22,7 +22,7 @@ export function stringifyError(
   return `${errorDescription}\n${
     err instanceof Error
       ? stringifyErrorValue(err)
-      : err
+      : String(err)
       ? '' + err
       : '(missing error information)'
   }`
